feb784e12132dec6c65dd5f7e44d0c9543f245fe
Andrys <aadam606@gmail.com>
2020-07-11 19:13:14 +0200

    added day/night and star location

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,15 @@
 const input = document.getElementById('volStars');
+const forest = document.getElementById('forestImg');
+const checkBox = document.getElementById('lightOn');
 const exit = document.getElementById('exit');
 const leftMenu = document.querySelector('.leftMenu');
 const rightIcon = document.querySelector('.rightIcon');
 const content = document.querySelector('.content');
+const moon = document.querySelector('.moon');
+const container = document.querySelector('.container');
 const inputValueNumber = document.getElementById('inputValueNumber');
 let inputValue = input.value;
-let enabled = false;
+let enabled = true;
 
 inputValueNumber.innerHTML = inputValue;
 
@@ -13,7 +17,7 @@ const openLeftMenu = () => {
     leftMenu.classList.add('transformStyleOpen');
     leftMenu.classList.remove('transformStyleClose');
 }
-const closeLeftMenu = async () => {
+const closeLeftMenu = () => {
     leftMenu.classList.remove('transformStyleOpen');
     leftMenu.classList.add('transformStyleClose');
 }
@@ -30,25 +34,50 @@ input.addEventListener("input", (e) => {
 
 const generateStars = (value) => {
     let numberOfStars = value;
+    if(enabled) {
+        for(let i = 0; i < numberOfStars; i++) {
+            let newStar = document.createElement('i');
+            let newDiv = document.createElement('div');
+            let newText = document.createElement('p');
+            let x = Math.floor(Math.random() * window.innerWidth);
+            let y = Math.floor(Math.random() * window.innerHeight);
+            let duration = Math.random() * 10;
+            let sizeOfStar = Math.random() * 2;
 
-    for(let i = 0; i < numberOfStars; i++) {
-        let newStar = document.createElement('i');
-        let x = Math.floor(Math.random() * window.innerWidth);
-        let y = Math.floor(Math.random() * window.innerHeight);
-        let duration = Math.random() * 10;
-        let sizeOfStar = Math.random() * 2;
+            newStar.style.width = 1 + sizeOfStar + 'px';
+            newStar.style.height = 1 + sizeOfStar + 'px';
+            newStar.style.top = y + 'px';
+            newStar.style.left = x + 'px';
 
-        newStar.style.width = 1 + sizeOfStar + 'px';
-        newStar.style.height = 1 + sizeOfStar + 'px';
-        newStar.style.top = y + 'px';
-        newStar.style.left = x + 'px';
+            newStar.style.animationDuration = 5 + duration + 's';
+            newStar.style.animationDelay = duration + 's';
 
-        newStar.style.animationDuration = 5 + duration + 's';
-        newStar.style.animationDelay = duration + 's';
+            newDiv.style.top = y + 10 + 'px';
+            newDiv.style.left = x - 75 + 'px';
+            
+            setInterval(() => {
+                newText.innerHTML = "<b>Top: </b>" + newStar.getBoundingClientRect().top + " " + "<b>Left: </b>" + newStar.offsetLeft; 
+            }, 500);
 
-        newStar.className = 'stars';
+            newDiv.style.animationDuration = newStar.style.animationDuration;
+            newDiv.style.animationDelay = newStar.style.animationDelay;
 
-        content.appendChild(newStar);
+            newStar.className = 'stars';
+            newDiv.className = 'starWindow';
+
+            newStar.addEventListener('click', () => {
+                window.removeEventListener('click', closeStarWindow);
+                newDiv.style.opacity = 1;
+                console.log('click');
+                setTimeout(() => {
+                    window.addEventListener('click', closeStarWindow)
+                }, 100)
+            });
+
+            content.appendChild(newDiv);
+            newDiv.appendChild(newText);
+            content.appendChild(newStar);
+        }
     }
 };
 const removeStars = () => {
@@ -64,3 +93,32 @@ window.addEventListener('resize', () => {
     removeStars();
     generateStars(inputValue);
 });
+
+checkBox.addEventListener('click', () => {
+    if (checkBox.checked == false) {
+        container.style.background = "";
+        moon.style.backgroundColor = "";
+        moon.style.boxShadow = "";
+        forest.src = "./images/forest.png";
+        enabled = true;
+        generateStars(inputValue);
+    } else {
+        container.style.background = "linear-gradient(#9be2fe, #67d1fb)";
+        moon.style.backgroundColor = "#f9d71c";
+        moon.style.boxShadow = "0px 0px 10px #f9d71c";
+        forest.src = "./images/forestDay.png";
+        removeStars();
+        enabled = false;
+    }
+});
+
+const closeStarWindow = (e) => {
+    console.log(e.target);
+    const divSelector = document.querySelectorAll('.starWindow');
+    for (let k = 0; k < divSelector.length; k++) {
+        console.log(divSelector[k].style.opacity)
+        divSelector[k].style.opacity = 0;
+    }
+}
+
+window.addEventListener('click', closeStarWindow);
